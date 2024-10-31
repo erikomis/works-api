5a0a417d0435594337c7c99e227ea8e992027b41
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-31 16:57:58 -0300

    feat:  entity

---
+++ src/module/product/entity/product.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity("products")
+export class Product {
+  @PrimaryGeneratedColumn("increment", { name: "id" })
+  id: number;
+  @Column("varchar", { length: 255, name: "plu" })
+  plu: string;
+
+  @Column("varchar", { length: 255, name: "description" })
+  description: string;
+
+  @Column("varchar", { length: 255, name: "ncm" })
+  ncm: string;
+
+  @Column("varchar", { length: 255, name: "unidade" })
+  unidade: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  public toString(): string {
+    return `Product [id=${this.id}, plu=${this.plu}, description=${this.description}, ncm=${this.ncm}, unidade=${this.unidade}, created_at=${this.created_at}]`;
+  }
+}
