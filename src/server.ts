cce44c87b7756c70ed796fdc54b32f966bd04d75
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-31 18:16:10 -0300

    fix :

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import "express-async-errors";
+import { env } from "@/common/utils/envConfig";
 import cors from "cors";
 import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
-import { env } from "@/common/utils/envConfig";
-import { ErrorHandle } from "./common/middleware/error-handler";
+import { ErrorHandler } from "./common/middleware/error-handler";
 import rateLimiter from "./common/middleware/rate-limiter";
 import requestLogger from "./common/middleware/request-logger";
 import { routeProduct } from "./routes/product.route";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
-const errorHandler = new ErrorHandle();
+const errorHandler = new ErrorHandler();
 
 // Middlewares
 app.use(express.json());
@@ -19,8 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
 app.use(rateLimiter);
-app.use(errorHandler.handle);
 app.use(requestLogger);
+
+app.use("/product", routeProduct);
+
+app.use(errorHandler.handle);
 
 app.use("/product", routeProduct);
 export { app, logger };
