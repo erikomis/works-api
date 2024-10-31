8279ca24b0e04d2e1a15da5b03154e75bfc634d1
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-31 16:41:29 -0300

    feat: use middlewares

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,20 @@ import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
 import { env } from "@/common/utils/envConfig";
-
-
+import { ErrorHandle } from "./common/middleware/error-handler";
+import rateLimiter from "./common/middleware/rate-limiter";
+import requestLogger from "./common/middleware/request-logger";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
-
+const errorHandler = new ErrorHandle();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
-
+app.use(rateLimiter);
+app.use(errorHandler.handle);
+app.use(requestLogger);
 export { app, logger };
